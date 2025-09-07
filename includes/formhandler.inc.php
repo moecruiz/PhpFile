9b4e104a3a9b09f3af362e44ffd8d1147f4fcceb
moecruiz <54955184+moecruiz@users.noreply.github.com>
2025-09-07 18:45:27 +0100

    php

---
+++ includes/formhandler.inc.php
@@ -0,0 +1,27 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"]== "POST"){
+    $username = $_POST["username"];
+    $pwd = $_POST["pwd"];
+    $email = $_POST["email"];
+
+    try{
+        require_once 'dbh.inc.php';
+
+        $query = "INSERT INTO users (username, pwd, email) VALUES (:username, :pwd, :email)";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':pwd', $pwd);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+
+        $pdo = null;
+        $stmt = null;
+        header("Location: ../index.php"); // Redirect after successful
+        die();
+    }catch(PDOException $e){
+        die( "Connection failed: " . $e->getMessage());
+    }
+}else{
+    header("Location: ../index.php");
+}
--- index.php
+++ index.php
@@ -8,65 +8,18 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <input type="number" name="num01" placeholder="Enter a number 1">
-        <select name ="operator">
-            <option value="add">+</option>
-            <option value="subtract">-</option>
-            <option value="multiply">*</option>
-            <option value="divide">/</option>
-            </select>
-            <input type="number" name="num02" placeholder="Enter a number 2">
-        <button>Calculate</button>
-    </form>
-        <?php 
-        if ($_SERVER["REQUEST_METHOD"]== "POST") {
-            // gets data from the form
-            $num01 = filter_input(INPUT_POST, 'num01', FILTER_SANITIZE_NUMBER_FLOAT);
-            $num02 = filter_input(INPUT_POST, 'num02', FILTER_SANITIZE_NUMBER_FLOAT);
-            $operator = htmlspecialchars($_POST['operator']);
-
-//error handlers
-            $errors = false;
-
-
-            if (empty($num01) || empty($num02) || empty($operator)) {
-                echo "<p class='error'>Please fill in all fields.</p>";
-                $errors = true;
-            }
-
-            if (!is_numeric($num01) || !is_numeric($num02)) {
-                echo "<p class='calc-error'>Please enter valid numbers.</p>";
-                $errors = true;
-            }
-
-            //calculate if no errors
-            if (!$errors) {
-                $value = 0; 
-                switch ($operator) {
-                    case 'add':
-                        $value = $num01 + $num02;
-                        break;
-                    case 'subtract':
-                        $value = $num01 - $num02;
-                        break;
-                    case 'multiply':
-                        $value = $num01 * $num02;
-
-                        break;
-                    case 'divide':
-                        $value = $num01 / $num02;
-                    default:
-                        echo "<p class='calc-error'>Something went wrong.</p>";
-                       
-                }
-                echo "<p class='calc-value'>Value=" . $value ."</p>";
-            }
-        }
-        // dont know why its not working 
     
-        
-        ?>
+<h3>Signup</h3>
+
+<form action = "includes/formhandler.inc.php" method = "POST">
+    <input type="text" name = "username" placeholder = "Username" >
+    
+    
+    <input type="password" name = "pwd" placeholder = "Password">
+
+    <input type = "text" name = "email" placeholder="E-mail">
+    <button>Signup</button>
+
 </body>
 
 
